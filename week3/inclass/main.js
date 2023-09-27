7989c69dadba3729baadd1af5542525e2f5d895f
mxymarcos <mxymarcos@gmail.com>
2023-09-26 19:07:53 -0700

    week3 in class practice

---
+++ week3/inclass/main.js
@@ -0,0 +1,24 @@
+let firstDiv = document.getElementById("divOne")
+let colorChangeButton = document.getElementById("btnChangeColor")
+let addTextButton = document.getElementById("btnAddText")
+
+console.log(colorChangeButton)
+
+let changeColor = function () {
+	let redComp = Math.random() * 255
+	let greenComp = Math.random() * 255
+	let blueComp = Math.random() * 255
+	let bgColor = "rgb(" + redComp + ", " + greenComp + ", " + blueComp + ")"
+	firstDiv.style.backgroundColor = bgColor
+}
+
+let addText = function () {
+	let someText = "Hello, this is some text"
+	let parag = document.createElement("p")
+	parag.innerText = someText
+	firstDiv.appendChild(parag)
+}
+
+firstDiv.style.backgroundColor = "rgb(230,170,170)"
+colorChangeButton.addEventListener("click", changeColor)
+addTextButton.addEventListener("click", addText)
